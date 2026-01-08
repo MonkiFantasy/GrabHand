ae52bc322d402e0f4e89f0db1c3564810e0357f0
MonkiFantasy <monki4536251@outlook.com>
2026-01-08 21:28:39 +0800

    feat: initialize project structure and Github Actions CI workflow

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+/// Handles camera input and frame acquisition.
+pub mod camera;
+/// Handles gesture recognition using the Mediapipe framework.
+pub mod gesture;
+/// Simulates user input for the Wayland display server.
+pub mod uinput_handler;
